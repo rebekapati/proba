4294d96f2e8dda665b5b16aaa7210bb41f8494a2
rebekapati <apati.rebeka@gmail.com>
2024-03-14 13:04:01 +0100

    gyak0314

--- HajósTeszt/HajósTeszt/Form1.cs
+++ HajósTeszt/HajósTeszt/Form1.cs
@@ -4,11 +4,28 @@ namespace HajósTeszt
     {
         List<Kérdés> ÖsszesKérdés;
         List<Kérdés> AktuálisKérdések = new List<Kérdés>();
+
+        //A válaszgomb nem jelent meg a toolboxban...
+        VálaszGomb VálaszGomb1;
+        VálaszGomb VálaszGomb2;
+        VálaszGomb VálaszGomb3;
+
         int AktuálisKérdés = 5;
 
         public Form1()
         {
             InitializeComponent();
+            VálaszGomb1 = new VálaszGomb();
+            VálaszGomb1.Top = 50;
+            Controls.Add(VálaszGomb1);
+
+            VálaszGomb2 = new VálaszGomb();
+            VálaszGomb2.Top = 150;
+            Controls.Add(VálaszGomb2);
+
+            VálaszGomb3 = new VálaszGomb();
+            VálaszGomb3.Top = 250;
+            Controls.Add(VálaszGomb3);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -20,7 +37,28 @@ namespace HajósTeszt
                 AktuálisKérdések.Add(ÖsszesKérdés[0]);
                 ÖsszesKérdés.RemoveAt(0);
             }
+
             dataGridView1.DataSource = AktuálisKérdések;
+
+            Kérdésmegjelenítés(AktuálisKérdések[AktuálisKérdés]);
+        }
+
+        void Kérdésmegjelenítés(Kérdés kérdés)
+        {
+            label1.Text = kérdés.KérdésSzöveg;
+            VálaszGomb1.Text = kérdés.Válasz1;
+            VálaszGomb2.Text = kérdés.Válasz2;
+            VálaszGomb3.Text = kérdés.Válasz3;
+
+            if (string.IsNullOrEmpty(kérdés.URL))
+            {
+                pictureBox1.Visible = false;
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Load("https://storage.altinum.hu/hajo/" + kérdés.URL);
+            }
         }
 
         List<Kérdés> KérdésBetöltés()
---
+++ HajósTeszt/HajósTeszt/VálaszGomb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajósTeszt
+{
+    internal class VálaszGomb : TextBox //magától tördeli a sorokat, nem kell manuálisan
+    {
+        public VálaszGomb()
+        {
+            ReadOnly = true; //csak olvasható lesz a tartalma
+            BackColor = Color.Gray;
+            BorderStyle = BorderStyle.None;
+            Multiline = true; //több soros
+            Height = 70;
+            Width = 300;
+            Left = 20;
+        }
+    }
+}
